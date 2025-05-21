2620791e7fa9629d2a019d4663096c316f939bd4
Boy Suganda Sinaga <boy.sinaga@shipper.id>
2025-05-21 21:08:22 +0700

    update pipeline

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -1,5 +1,8 @@
 """
-AWS Security Scanner package.
+AWS Security Scanner Package
+This package contains modules for scanning AWS resources for security issues.
 """
+
+__version__ = "0.1.0"
 
 # empty
